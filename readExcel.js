15a8ab2c0ac5faa337760201f0c867b995198194
rah99 <rah99.bth@gmail.com>
2022-02-16 22:03:35 +0000

    Counting keys

--- readExcel.js
+++ readExcel.js
@@ -1,8 +1,18 @@
-import { readFile, utils } from "xlsx"
+var xlsx = require("xlsx")
 var dataPathExcel = "testExcel.xlsx"
-var wb = readFile(dataPathExcel)
+var wb = xlsx.readFile(dataPathExcel)
 var sheetName = wb.SheetNames[0]
 var sheetValue = wb.Sheets[sheetName]
 // console.log(sheetValue)
-let excelData = utils.sheet_to_json(sheetValue)
+let excelData = xlsx.utils.sheet_to_json(sheetValue)
 console.log(excelData)
+
+occurrance = {}
+excelData.forEach(function (o) {
+    if (!occurrance.hasOwnProperty(o.Sno)) {
+        occurrance[o.Sno] = 0;
+    }
+    occurrance[o.Sno] += 1;
+})
+
+console.log(occurrance);
