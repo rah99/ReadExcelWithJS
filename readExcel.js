a2d71f525800c94c36b67081a80d6e53f0154f47
rah99 <rah99.bth@gmail.com>
2022-02-17 03:49:25 +0000

    Return max count for Json entity.

--- readExcel.js
+++ readExcel.js
@@ -5,14 +5,25 @@ var sheetName = wb.SheetNames[0]
 var sheetValue = wb.Sheets[sheetName]
 // console.log(sheetValue)
 let excelData = xlsx.utils.sheet_to_json(sheetValue)
-console.log(excelData)
+// console.log(excelData)
 
-occurrance = {}
+var occurrance = {}
 excelData.forEach(function (o) {
-    if (!occurrance.hasOwnProperty(o.Sno)) {
-        occurrance[o.Sno] = 0;
+    if (!occurrance.hasOwnProperty(o.Name)) {
+        occurrance[o.Name] = 0;
     }
-    occurrance[o.Sno] += 1;
+    occurrance[o.Name] += 1;
 })
 
-console.log(occurrance);
+// console.log(occurrance);
+
+function getKeysWithHighestValue(o, n) {
+    var keys = Object.keys(o);
+    keys.sort(function (a, b) {
+        return o[b] - o[a];
+    })
+    // console.log(keys);
+    return keys.slice(0, n);
+}
+
+console.log(getKeysWithHighestValue(occurrance, 1))
