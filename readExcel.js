bde40f83b111fe4d096052079b06ea9b03798678
rah99 <rah99.bth@gmail.com>
2022-02-18 00:02:34 +0000

    Trying different spreadsheets.

--- readExcel.js
+++ readExcel.js
@@ -1,21 +1,21 @@
 var xlsx = require("xlsx")
-var dataPathExcel = "testExcel.xlsx"
+var dataPathExcel = "HiredCandidateCaseStudy.xlsx"
 var wb = xlsx.readFile(dataPathExcel)
-var sheetName = wb.SheetNames[0]
+var sheetName = wb.SheetNames[3]
 var sheetValue = wb.Sheets[sheetName]
 // console.log(sheetValue)
 let excelData = xlsx.utils.sheet_to_json(sheetValue)
-// console.log(excelData)
+console.log(excelData)
 
 var occurrance = {}
 excelData.forEach(function (o) {
-    if (!occurrance.hasOwnProperty(o.Name)) {
-        occurrance[o.Name] = 0;
+    if (!occurrance.hasOwnProperty(o.Benefit1)) {
+        occurrance[o.Benefit1] = 0;
     }
-    occurrance[o.Name] += 1;
+    occurrance[o.Benefit1] += 1;
 })
 
-// console.log(occurrance);
+console.log(occurrance);
 
 function getKeysWithHighestValue(o, n) {
     var keys = Object.keys(o);
